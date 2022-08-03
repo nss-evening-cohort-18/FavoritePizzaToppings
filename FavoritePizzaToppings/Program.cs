142803771b469568c37f4e131938c7e5abe36a4b
Brian Neal <brianbneal@gmail.com>
2022-08-02 21:15:26 -0500

    found the most popular pizza, hoorah

--- FavoritePizzaToppings/Program.cs
+++ FavoritePizzaToppings/Program.cs
@@ -10,18 +10,38 @@ using Newtonsoft.Json;
 //Check this link [to read a JSON file] (https://www.newtonsoft.com/json/help/html/DeserializeWithJsonSerializerFromFile.htm) in c#.
 //You'll need to install the Newtonsoft.Json nuget package, and create a class that represents the pizzas.
 
-
 //one possible way to deserialize JSON
 // read file into a string and deserialize JSON to a type
-string result = File.ReadAllText("C:\\Users\\Neal\\source\\repos\\E18\\FavoritePizzaToppings\\FavoritePizzaToppings\\pizzas (1).json");
-var pizzas = JsonConvert.DeserializeObject<List<Pizza>>(result);
+string jsonText = File.ReadAllText(@"C:\Users\Neal\source\repos\E18\FavoritePizzaToppings\FavoritePizzaToppings\pizzas (1).json");
+var pizzas = JsonConvert.DeserializeObject<List<Pizza>>(jsonText);
 
 //another possible way
 // deserialize JSON directly from a file
-using (StreamReader file = File.OpenText(@"C:\Users\Neal\source\repos\E18\FavoritePizzaToppings\FavoritePizzaToppings\pizzas (1).json"))
+//using (StreamReader file = File.OpenText(@"C:\Users\Neal\source\repos\E18\FavoritePizzaToppings\FavoritePizzaToppings\pizzas (1).json"))
+//{
+//    JsonSerializer serializer = new JsonSerializer();
+//    var pizzas2 = serializer.Deserialize(file, typeof(List<Pizza>)) as List<Pizza>;
+//}
+
+//Dictionary<List<string>, int> timesOrdered = new Dictionary<List<string>, int>(IEqualityComparer<List<string>>.Equals());
+Dictionary<string, int> toppingsCounter = new Dictionary<string, int>();
+
+pizzas.ForEach(pizza =>
 {
-    JsonSerializer serializer = new JsonSerializer();
-    var pizzas2 = serializer.Deserialize(file, typeof(List<Pizza>)) as List<Pizza>;
-}
+    string toppingsAsString = String.Join(",", pizza.Toppings.OrderByDescending(x => x));
+    if (!toppingsCounter.ContainsKey(toppingsAsString))
+    {
+        toppingsCounter[toppingsAsString] = 1;
+    }
+    else
+    {
+        toppingsCounter[toppingsAsString]++;
+    };
+});
+
+var mostPopularPizzaToppings = toppingsCounter.OrderByDescending(toppingCombo => toppingCombo.Value).Take(1).FirstOrDefault();
+var mostTimesOrdered = toppingsCounter.Max(item => item.Value);
+Console.WriteLine($"The most popular pizza was ordered {mostTimesOrdered} times.");
+Console.WriteLine($"The most popular pizza is {mostPopularPizzaToppings.Key}, and it was ordered {mostPopularPizzaToppings.Value} times.");
 
 Console.ReadLine();
